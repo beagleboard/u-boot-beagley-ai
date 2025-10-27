afa7ac95721179c703f2865170193387da812e61
Robert Nelson <robertcnelson@gmail.com>
2025-10-26 19:21:43 -0500

    bump: OPTEE:4.8.0, TFA:lts-v2.12.7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -5,10 +5,10 @@ TI_FIRMWARE="${TI_FIRMWARE:-11.01.17}"
 
 #https://github.com/TrustedFirmware-A/trusted-firmware-a.git
 ATF_GIT="${ATF_GIT:-https://github.com/TrustedFirmware-A/trusted-firmware-a.git}"
-ATF="lts-v2.12.6"
+ATF="lts-v2.12.7"
 
 #https://github.com/OP-TEE/optee_os.git
-OPTEE="4.7.0"
+OPTEE="4.8.0"
 
 #https://github.com/beagleboard/u-boot.git
 UBOOT_GIT="${UBOOT_GIT:-https://github.com/beagleboard/u-boot.git}"
