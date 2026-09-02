21c0ff2a30c7073893e62df454547c8c9d95f1e2
Robert Nelson <robertcnelson@gmail.com>
2026-09-02 11:55:53 -0500

    ci: use local registry
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# SPDX-FileCopyrightText: Robert Nelson <robertcnelson@gmail.com>
+# SPDX-License-Identifier: MIT
+
 git pull --no-edit https://github.com/beagleboard/u-boot-beagley-ai.git main
+git pull --no-edit https://gitlab.com/beagle-pkgs/u-boot-beagley-ai.git main
