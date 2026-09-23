c031a52ac2690029f68093e6eae750488b0eb371
Robert Nelson <robertcnelson@gmail.com>
2026-09-23 14:34:26 -0500

    ci: rework tag to include TFA tag
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/push-tag.sh
+++ scripts/push-tag.sh
@@ -9,12 +9,12 @@ echo [${UBOOT}:${TFA}:${OPTEE}:${TI_FIRMWARE}]
 echo "****************************************************"
 
 cp public/base.sh public/get_n_install.sh
-sed -i -e 's:TAG:'${UBOOT}'-'${TI_FIRMWARE}':g' public/get_n_install.sh
+sed -i -e 's:TAG:'${UBOOT}'-'${TFA}'-'${TI_FIRMWARE}':g' public/get_n_install.sh
 
-echo "${UBOOT}-${TI_FIRMWARE} release" > ${wfile}
+echo "${UBOOT}-${TFA}-${TI_FIRMWARE} release" > ${wfile}
 
 git commit -a -F ${wfile} -s
 
-git tag -a ${UBOOT}-${TI_FIRMWARE} -m "${UBOOT}-${TI_FIRMWARE}"
+git tag -a ${UBOOT}-${TFA}-${TI_FIRMWARE} -m "${UBOOT}-${TFA}-${TI_FIRMWARE}"
 
 git push origin main --tags
