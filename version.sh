2b71ecffdb44643313b286e7fc0ada54982dd931
Robert Nelson <robertcnelson@gmail.com>
2026-09-23 15:57:25 -0500

    bump: TFA:-lts-v2.14.8
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -9,10 +9,10 @@ TI_FIRMWARE_GIT="${TI_FIRMWARE_GIT:-https://github.com/TexasInstruments/ti-linux
 TI_FIRMWARE="${TI_FIRMWARE:-12.02.00.01}"
 
 #https://github.com/TrustedFirmware-A/trusted-firmware-a.git
-#https://github.com/TrustedFirmware-A/trusted-firmware-a/compare/lts-v2.14.6...lts-v2.14.7
-#https://forgejo.gfnd.rcn-ee.org:3000/mirror/trusted-firmware-a/compare/lts-v2.14.6...lts-v2.14.7
+#https://github.com/TrustedFirmware-A/trusted-firmware-a/compare/lts-v2.14.7...lts-v2.14.8
+#https://forgejo.gfnd.rcn-ee.org:3000/mirror/trusted-firmware-a/compare/lts-v2.14.7...lts-v2.14.8
 TFA_GIT="${TFA_GIT:-https://github.com/TrustedFirmware-A/trusted-firmware-a.git}"
-TFA="${TFA:-lts-v2.14.7}"
+TFA="${TFA:-lts-v2.14.8}"
 
 #https://github.com/OP-TEE/optee_os.git
 #https://github.com/OP-TEE/optee_os/compare/4.9.0...4.10.0
